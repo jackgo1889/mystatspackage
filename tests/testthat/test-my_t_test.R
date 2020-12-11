da071304d5099c23d8d30eb8e8f6adfb42eae5b6
Jack Go <jackgo1998@gmail.com>
2020-12-10 19:38:35 -0800

    fixed the tests for my_t_test

--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -1,10 +1,16 @@
 test_that("my_t_test works mathematically", {
   test_vector <- c(1:25)
   results <- my_t_test(test_vector, alternative = "less", mu = 14)
-  expect_equal(results$test_stat, -0.679366)
-  expect_equal(results$df, 24)
-  expect_match(results$alternative, "less")
-  expect_equal(results$p_val, 0.25170)
+  test_1 <- results$`test statistic`
+  test_1 <- round(test_1, digits = 6)
+  test_2 <- results$`degrees of freedom`
+  test_3 <- results$`alternative hypothesis`
+  test_4 <- results$p_value
+  test_4 <- round(test_4, digits = 6)
+  expect_equal(test_1, -0.679366)
+  expect_equal(test_2, 24)
+  expect_match(test_3, "less")
+  expect_equal(test_4, 0.251703)
 })
 test_that("Unspecified string throws error", {
   test_vector <- c(1:25)
