84854bf3700e931d77b1938eae7856d0cad6a640
Jack Go <jackgo1998@gmail.com>
2020-12-14 16:03:08 -0800

    updated tests.

--- tests/testthat/test-my_knn_cv.R
+++ tests/testthat/test-my_knn_cv.R
@@ -1,4 +1,4 @@
-test_that("my_knn_cv works mathematically", {
+test_that("my_knn_cv outputs correct classes", {
   data(my_penguins)
   penguins_df <- tidyr::drop_na(my_penguins)
   results <- my_knn_cv(penguins_df[ , 3:6], penguins_df$species, 5, 5)
--- tests/testthat/test-my_rf_cv.R
+++ tests/testthat/test-my_rf_cv.R
@@ -1,4 +1,4 @@
-test_that("my_rf_cv works mathematically", {
+test_that("my_rf_cv outputs a numeric", {
   results <- my_rf_cv(5)
   expect_is(results, "numeric")
 })
